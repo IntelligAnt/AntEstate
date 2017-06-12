e804027eade3aa10192b2d4957e3e9ef6d055991
Konstantin Đorđević <vomindoraan@gmail.com>
2017-06-13 01:34:44 +0200

    Separate out 'Call to Action'

--- src/js/header-footer.js
+++ src/js/header-footer.js
@@ -2,5 +2,6 @@ $(function() {
 	/* Load header and footer
     ======================================================*/
     $("#header").load("header.html");
+    $("#action").load("action.html");
     $("#footer").load("footer.html");
 });
