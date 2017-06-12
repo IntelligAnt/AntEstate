99a0de74dbf9338bf104543ac0565a6872c596bc
Konstantin Đorđević <vomindoraan@gmail.com>
2017-06-12 22:47:36 +0200

    Load header & footer with jQuery

---
+++ src/js/header-footer.js
@@ -0,0 +1,6 @@
+$(function() {
+	/* Load header and footer
+    ======================================================*/
+    $("header").load("header.html");
+    $("footer").load("footer.html");
+});
