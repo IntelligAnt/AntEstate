c99f103ff1a0bd729ac7c4c64d063e55e0570eb7
Natalija Radic <natalijaradic0401@yahoo.com>
2017-06-13 04:18:11 +0200

    Add favourites

---
+++ src/js/favourites.js
@@ -0,0 +1,30 @@
+function showFilteredData(data) {
+    document.querySelector("#number-of-results").innerHTML = data.length + " favourites";
+    var templateSource = document.querySelector("#entry-template").innerHTML;
+    var compiledTemplate = Handlebars.compile(templateSource);
+    document.querySelector("#grid-view-parent").innerHTML = "";
+    for (var property in data) {
+        document.querySelector("#grid-view-parent").innerHTML += compiledTemplate(data[property]);
+    }
+}
+
+var favs = [0, 1, 2];
+var f_data = [];
+getDataFromArray(favs);
+
+function getDataFromArray(arr) {
+    $.get("properties.json", function (data) {
+        for (var i = 0; i < arr.length; i++) {
+            var property = data.find(function (e) {
+                return e.id == arr[i];
+            });
+            if (property !== undefined) f_data.push(property);
+        }
+        showFilteredData(f_data);
+    });
+}
+
+function sortClick(type) {
+    showFilteredData(sort(f_data, type));
+}
+
